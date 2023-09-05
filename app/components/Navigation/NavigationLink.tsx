aaa14d6d4d892291b0aa486d7e6bb39144ccc3e2
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-05 21:21:44 +0200

    feat: Reusable Link component

--- app/components/Navigation/NavigationLink.tsx
+++ app/components/Navigation/NavigationLink.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { AnchorHTMLAttributes } from 'react';
 import { Link as UILink } from '@nextui-org/link';
 import Link from 'next/link';
 
-type Props = {
+interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
 	className?: string;
 	href: string;
 	children: React.ReactNode;
-};
+}
 
 export default function NavigationLink({ href, className, children, ...restProps }: Props) {
 	return (
 		<Link {...restProps} href={href}>
-			<UILink>{children}</UILink>
+			<UILink className="">{children}</UILink>
 		</Link>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import BoundWrapper from '@/app/components/UI/BoundWrapper';
 
 export default function Home() {
 	return (
-		<BoundWrapper className="grid grid-cols-2">
+		<BoundWrapper className="">
 			<div className="">
 				<Heading as="h1">H1</Heading>
 				<Heading as="h2">H2</Heading>
@@ -15,20 +15,6 @@ export default function Home() {
 				<p>body</p>
 				<p className="light">body light</p>
 				<p className="light sm">body light sm</p>
-				<p className="display1">Display 1</p>
-				<p className="display2">Display 2</p>
-				<NavigationLink href="/">NavLink/Button</NavigationLink>
-			</div>
-			<div className="">
-				<Heading as="h1">H1</Heading>
-				<Heading as="h2">H2</Heading>
-				<Heading as="h3">H3</Heading>
-				<Heading as="h4">H4</Heading>
-				<Heading as="h5">H5</Heading>
-				<Heading as="h6">H6</Heading>
-				<p>body</p>
-				<p className="light">body light</p>
-				<p className="light ">body light sm</p>
 				<p className="display1">Display 1</p>
 				<p className="display2">Display 2</p>
 				<NavigationLink href="/">NavLink/Button</NavigationLink>
