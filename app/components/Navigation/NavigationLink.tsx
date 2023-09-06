e28e48a702d73c0aa97f2c2200755cd1f23ae1de
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-06 13:47:00 +0200

    bug: remove duplicate anchor tags

--- app/components/Navigation/NavigationLink.tsx
+++ app/components/Navigation/NavigationLink.tsx
@@ -1,17 +1,12 @@
 import React, { AnchorHTMLAttributes } from 'react';
-import { Link as UILink } from '@nextui-org/link';
 import Link from 'next/link';
 
-interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
 	className?: string;
 	href: string;
 	children: React.ReactNode;
 }
 
 export default function NavigationLink({ href, className, children, ...restProps }: Props) {
-	return (
-		<Link {...restProps} href={href}>
-			<UILink className="">{children}</UILink>
-		</Link>
-	);
+	return <Link {...restProps} href={href}></Link>;
 }
