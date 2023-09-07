b0a7b35df051c3cffd3e2b68a6afb974b88b3182
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-07 20:35:14 +0200

    config: page test setup for links

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,21 @@ import { prismicClient } from '@/app/lib/clients';
 
 export default async function Home() {
 	const { data } = await prismicClient.getSingle('navigation');
-
 	return (
-		<BoundWrapper>
-			<NavigationLink className="m-5" field={data.cta_link}>
+		<BoundWrapper className="h-[300vh] flex flex-col justify-between">
+			<div className="fixed top-10 left-20">
+				<NavigationLink className="m-5 " field={data.cta_link}>
+					{data.cta_label}
+				</NavigationLink>
+			</div>
+
+			<NavigationLink className="m-5" href="#contact-me">
 				{data.cta_label}
 			</NavigationLink>
+			<NavigationLink className="m-5" href="/contact-me">
+				{data.cta_label}
+			</NavigationLink>
+			<div className="h-screen w-full bg-blue-300" id="contact-me"></div>
 		</BoundWrapper>
 	);
 }
