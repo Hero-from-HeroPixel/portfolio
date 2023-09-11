e3d25fb42070fd0f6f3b2fdbe05d94fcd23a5bf8
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-11 11:15:28 +0200

    feat: add slice from Slice Machine with fallback

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,21 @@ import BoundWrapper from '@/app/components/UI/BoundWrapper';
 
 import { prismicClient } from '@/app/lib/clients';
 import Hero from '@/app/components/elements/Hero';
+import { SliceZone } from '@prismicio/react';
+import { components } from '@/slices';
+import { Metadata } from 'next';
 
 export default async function Home() {
-	const { data } = await prismicClient.getSingle('navigation');
+	let pageData;
+	try {
+		const { data } = await prismicClient.getSingle('home_page');
+		pageData = data;
+	} catch (error) {
+		console.error('failed to fetch CMS data');
+	}
+
+	if (pageData) return <SliceZone slices={pageData.slices} components={components} />;
+
 	return (
 		<>
 			<Hero />
@@ -14,4 +26,18 @@ export default async function Home() {
 			<div className="h-screen w-full " id="contact-me"></div>
 		</>
 	);
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+	let metaData;
+	try {
+		metaData = await prismicClient.getSingle('home_page');
+	} catch (error) {
+		console.error('failed to fetch Metadata');
+	}
+
+	return {
+		title: metaData?.data.meta_title || 'HeroPixel Portfolio',
+		description: metaData?.data.meta_description || 'Portfolio of JJ Grobler',
+	};
 }
