393df30b7ad7eef11e8ef3d6e0a84f9dc4759463
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-13 11:10:31 +0200

    feat: Add prismic slot for timeline

--- app/components/elements/About_Me/Timeline.tsx
+++ app/components/elements/About_Me/Timeline.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import Job from './Job';
 import { prismicClient } from '@/app/lib/clients';
 import styles from '@/app/components/elements/About_Me/timeline.module.css';
+import { JobDocument } from '@/prismicio-types';
 
-type Props = {};
+type Props = {
+	jobs?: JobDocument<string>[] | undefined;
+};
 
-export default async function Timeline({}: Props) {
-	let jobs;
-	try {
-		jobs = await prismicClient.getAllByType('job');
-	} catch (error) {
-		console.error('Failed to fetch Jobs');
+export default async function Timeline({ jobs }: Props) {
+	if (!jobs) {
+		try {
+			jobs = await prismicClient.getAllByType('job');
+		} catch (error) {
+			console.error('Failed to fetch Jobs');
+		}
 	}
+
 	return (
 		<div className="flex relative">
 			<div className={styles.timeline}></div>
