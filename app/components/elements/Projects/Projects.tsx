d95d14cbd45d265bd180059cb382d04109a4d02b
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-19 09:18:59 +0200

    feat: refactor slices

--- app/components/elements/Projects/Projects.tsx
+++ app/components/elements/Projects/Projects.tsx
@@ -6,16 +6,32 @@ import { prismicClient } from '@/app/lib/clients';
 import ProjectCard from './ProjectCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCodeBranch, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { ProjectsProps } from '@/slices/Projects';
+import { Content } from '@prismicio/client';
 
 type Props = {
 	id?: string;
+	featuredProject?: Content.ProjectDocument<string>;
+	mainProjects?: (Content.ProjectDocument<string> | undefined)[];
+	sideProjects?: (Content.ProjectDocument<string> | undefined)[];
 };
 
-export default async function Projects({ id }: Props) {
-	const featuredProject = await prismicClient.getByUID('project', 'enerblu.co.za');
-	let projects = await prismicClient.getAllByType('project');
-	const mainProjects = projects.filter((project) => project.tags.includes('Main Project'));
-	const sideProjects = projects.filter((project) => project.tags.includes('Side Project'));
+export default async function ProjectsSlice({ id }: Props): Promise<JSX.Element> {
+	let featuredProject;
+	let mainProjects;
+	let sideProjects;
+	if (!featuredProject)
+		featuredProject = await prismicClient.getByUID('project', 'enerblu.co.za');
+
+	let projects;
+	if (!mainProjects || !sideProjects) projects = await prismicClient.getAllByType('project');
+
+	if (!sideProjects && projects)
+		sideProjects = projects.filter((project) => project.tags.includes('Side Project'));
+
+	if (!mainProjects && projects)
+		mainProjects = projects.filter((project) => project.tags.includes('Main Project'));
+
 	return (
 		<section className="w-screen flex flex-col gap-20" id={id}>
 			<BoundWrapper as="div">
@@ -26,12 +42,13 @@ export default async function Projects({ id }: Props) {
 					Featured Project - Enerblu
 				</Heading>
 			</BoundWrapper>
-			<FeaturedProject project={featuredProject} />
-			<BoundWrapper as="div" className="gap-40">
+			{featuredProject && <FeaturedProject project={featuredProject} />}
+			<BoundWrapper as="div" className="gap-40 px-5">
 				<ul className="flex flex-col lg:flex-row gap-10 lg:gap-16 lg:justify-between">
-					{mainProjects.map((project) => (
-						<ProjectCard project={project} key={project.id} />
-					))}
+					{mainProjects &&
+						mainProjects.map((project) => (
+							<ProjectCard project={project} key={project.id} />
+						))}
 				</ul>
 				<div className="flex flex-col gap-10">
 					<Heading className="flex items-center" as="h3">
@@ -52,9 +69,10 @@ export default async function Projects({ id }: Props) {
 						</span>{' '}
 					</Heading>
 					<ul className="flex flex-col lg:flex-row gap-16 lg:gap-20">
-						{sideProjects.map((project) => (
-							<ProjectCard key={project.id} project={project} minify />
-						))}
+						{sideProjects &&
+							sideProjects.map((project) => (
+								<ProjectCard key={project.id} project={project} minify />
+							))}
 					</ul>
 				</div>
 			</BoundWrapper>
