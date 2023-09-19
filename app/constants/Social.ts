4e29e50131e6fe78b6ec93ee2ce2b536090902e0
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-19 11:06:41 +0200

    bug: cv link fallback

--- app/constants/Social.ts
+++ app/constants/Social.ts
@@ -3,4 +3,4 @@ export const MAIL_LINK = 'mailto:dev@heropixel.co.za';
 export const PHONE_LINK = 'tel:+27607326830';
 export const LINKDN_LINK = 'https://www.linkedin.com/in/jean-jacques-grobler/';
 export const RESUME_LINK =
-	'https://res.cloudinary.com/dfzac1yrx/image/upload/v1694435333/J_Grobler_2023_az4wir.pdf';
+	'https://res.cloudinary.com/dfzac1yrx/image/upload/v1695114371/J_Grobler_CV_2023_v2_qiz8cm.pdf';
