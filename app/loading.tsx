8ffc3731d3da0dfdbb98e2e179c5976c117075ae
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-30 16:45:02 +0200

    anim: update timing

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,6 +3,7 @@ import styles from '@/app/loader.module.css'
 export default function Loading() {
 	// Or a custom loading skeleton component
 	return (<div className="flex items-center w-full min-h-screen justify-center gap-4">
+		<p>Hi, I am</p>
 		<span className={styles.loader}></span>
 		</div>);
 }
