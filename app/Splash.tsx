889da750fa5caa45a807c23009a19aeeb70729da
Jean-Jacques Grobler <groblersales@gmail.com>
2023-10-04 22:28:06 +0200

    bug: fix useEffect cleanup

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -10,6 +10,8 @@ export default function Splash({}: Props) {
 
 	useEffect(() => {
 		const animHandler = setTimeout(animCompleteHandler, 3000);
+
+		return () => clearTimeout(animHandler);
 	}, []);
 	return (
 		<>
